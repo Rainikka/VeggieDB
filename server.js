6669bf3577fc2429d038a51c013d96442f32610b
Rainikka <rainikka@gmail.com>
2025-04-25 11:26:03 -0400

    /*** Set-Up: Middleware ***/

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-/*** Secured Connection String ***/
+
+/*********** ENVIRONMENT SET-UP ***********/
+/******************************************/
+
+/*** Secure: Connection String ***/
 const dotenv = require("dotenv");
 dotenv.config();
 
-/*** Set-Up Expres Server ***/
+/*** Set-Up: Express Server ***/
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
+/*** Set-Up: Middleware ***/
+app.use(express.urlencoded());
+app.use(express.json());
 
-const mongoose = require("mongoose")
+/*** Set-Up: Datatases: MongoDB & Mongoose ***/
+const mongoDB = require("mongodb");
+const mongoose = require("mongoose");
+
+/*** Database Models & Connections ***/
+const Fruit = require('./models/Fruit.js');
 
 app.listen(PORT, () => {
   console.log(`Server is listening on Port: André${PORT}`)
