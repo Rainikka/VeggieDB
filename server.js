08254c7725856245a49a601149f462508e71d941
Rainikka <rainikka@gmail.com>
2025-04-25 11:41:17 -0400

    /*** Require: Database Models ***/

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/****** Rainikka Corprew ******/
+/********* JAVASCRIPT *********/
+/******** ALAB 319.5 **********/
+/******************************/
+
+/******************************/
+/****** EXPRESS SERVER ********/
+/******** APPLICATION *********/
+/******** 17-APR-2025 *********/
 
 /*********** ENVIRONMENT SET-UP ***********/
 /******************************************/
@@ -6,21 +15,31 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-/*** Set-Up: Express Server ***/
+/*** Require: Express Server ***/
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-/*** Set-Up: Middleware ***/
-app.use(express.urlencoded());
+/*** Require: Middleware ***/
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/*** Set-Up: Datatases: MongoDB & Mongoose ***/
+/*** Require: MongoDB & Mongoose Datatases ***/
 const mongoDB = require("mongodb");
 const mongoose = require("mongoose");
 
-/*** Database Models & Connections ***/
-const Fruit = require('./models/Fruit.js');
+/*** Require: Database Models ***/
+const Veggie = require('./models/Veggie.js');
+const Veggies = require('./routes/Veggies.js');
+
+/*** Require: Database Connection ***/
+// mongoose.connect(process.env.ATLAS_URI)
+// mongoose.connection.once('open', () => {
+//   console.log('connected to mongoDB')
+// })
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on Port: André${PORT}`)
