38322ddb6f78d50ccae97bf806be6405b39cd918
Rainikka <rainikka@gmail.com>
2025-04-25 11:07:14 -0400

    /*** Set-Up Expres Server ***/

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 /*** Secured Connection String ***/
-const dotenv = reuiqre("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
+
+/*** Set-Up Expres Server ***/
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+const mongoose = require("mongoose")
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port: André${PORT}`)
+});
