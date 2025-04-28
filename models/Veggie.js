f1c1e4c3a085111eec4515b531c26a7fe76ac7cd
Rainikka <48374418+Rainikka@users.noreply.github.com>
2025-04-28 05:01:57 -0400

    Delete models directory

--- models/Veggie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const veggieSchema = new mongoose.Schema({
-  name: String,
-  color: String,
-  readyToEat: Boolean
-});
-
-module.exports = mongoose.model('Veggiee', veggieeSchema);
